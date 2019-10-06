0d5ea2f3cd2a46679d633973d1dd715076ddd164
harsha-iiiv <31560965+harsha-iiiv@users.noreply.github.com>
2019-10-06 11:56:12 +0530

    added index file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connectDB = require('./config/db')
+const index = require('./routes/index')
+const URL = require('./routes/url');
+const app = express();
+
+
+// connect DB
+
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/', index );
+app.use('/api/url', URL)
+
+const PORT = 5001 || process.env.PORT
+
+app.listen(PORT, ()=>
+console.log(`server running on ${PORT}`)
+)
